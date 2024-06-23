cd9272921e296c7af3759c6c79a2a045d7bdfdb8
Eva <as45986@gmail.com>
2024-06-23 13:35:43 +0800

    Fix formatting in About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,6 @@
 export default function About() {
   return (
+    // max-w-6xl 和 header 同寬
     <div className='max-w-6xl mx-auto p-3 space-y-4'>
       <h1 className='text-2xl font-medium text-amber-600'>About</h1>
       <p>
@@ -8,12 +9,12 @@ export default function About() {
         all your movie-related needs.
       </p>
 
-      <p>
+      <p >
         Our website is designed to provide you with a comprehensive database of
         movies from all around the world, along with the latest news, reviews,
-        and trailers. Our movie database is constantly updated with new
+        and trailers.<span className="text-rose-600"> Our movie database is constantly updated with new
         releases, ensuring that you have access to the latest and greatest in
-        the world of cinema. You can search for movies by title, director,
+        the world of cinema.</span> You can search for movies by title, director,
         actor, genre, or release date, making it easy to find the perfect movie
         for any occasion.
       </p>
