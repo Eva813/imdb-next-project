0a429851191c7f78ba440121ef7f2b917a4c8eeb
Eva <as45986@gmail.com>
2024-06-27 21:01:47 +0800

    fix: fix loading component and add results to home page

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,9 @@
 export default function loading() {
   return (
     <div className='flex justify-center mt-16'>
-      {/* <img className='h-52' src='spinner.svg' alt='loading...' /> */}
+      <picture>
+        <img className='h-52' src='spinner.svg' alt='loading...' />
+      </picture>
     </div>
   );
 }
--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,48 +1,29 @@
 'use client';
 // 因為會 add some hooks,to detect the system or the users is light or dark, 所以要加上 'use client';
 import { MdLightMode, MdDarkMode } from 'react-icons/md';
+
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
-export default function DarkModeSwitch(){
-  // useTheme() 來取得當下主題、和切換主題，會 useContext(ThemeContext) 來取得當下主題、和切換主題
+export default function DarkModeSwitch() {
   const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
   const currentTheme = theme === 'system' ? systemTheme : theme;
-  return(
+  useEffect(() => setMounted(true), []);
+  return (
     <div>
-      {currentTheme === 'dark' ? (
-        <MdLightMode
-          onClick={() => setTheme('light')}
-          className='text-xl cursor-pointer hover:text-amber-500'
-        />
-      ) : (
-        <MdDarkMode
-          onClick={() => setTheme('dark')}
-          className='text-xl cursor-pointer hover:text-amber-500'
-        />
-      )}
+      {mounted &&
+        (currentTheme === 'dark' ? (
+          <MdLightMode
+            onClick={() => setTheme('light')}
+            className='text-xl cursor-pointer hover:text-amber-500'
+          />
+        ) : (
+          <MdDarkMode
+            onClick={() => setTheme('dark')}
+            className='text-xl cursor-pointer hover:text-amber-500'
+          />
+        ))}
     </div>
-  )
+  );
 }
-
-// export default function DarkModeSwitch() {
-//   const { theme, setTheme, systemTheme } = useTheme();
-//   const [mounted, setMounted] = useState(false);
-//   const currentTheme = theme === 'system' ? systemTheme : theme;
-//   useEffect(() => setMounted(true), []);
-//   return (
-//     <div>
-//       {mounted &&
-//         (currentTheme === 'dark' ? (
-//           <MdLightMode
-//             onClick={() => setTheme('light')}
-//             className='text-xl cursor-pointer hover:text-amber-500'
-//           />
-//         ) : (
-//           <MdDarkMode
-//             onClick={() => setTheme('dark')}
-//             className='text-xl cursor-pointer hover:text-amber-500'
-//           />
-//         ))}
-//     </div>
-//   );
-// }
