64312dc7716c92f24c169c1e0b528562ae3ffb90
Eva <as45986@gmail.com>
2024-06-29 13:52:07 +0800

    fix: Refactor Card component styles and add movie poster alt text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,28 +4,29 @@ import { FiThumbsUp } from 'react-icons/fi';
 
 export default function Card({ result }) {
   return (
-    <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
+    <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-sm rounded-lg sm:border sm:border-slate-400 transition-shadow duration-300 sm:m-2'>
       <Link href={`/movie/${result.id}`}>
-        {/* <Image
+        <Image 
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
           }`}
+          alt='movie poster'
           width={500}
           height={300}
-          className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
-        ></Image> */}
-        <div className='p-2'>
-          <p className='line-clamp-2 text-md'>{result.overview}</p>
-          <h2 className='text-lg font-bold truncate'>
-            {result.title || result.name}
-          </h2>
-          <p className='flex items-center'>
-            {result.release_date || result.first_air_date}
-            <FiThumbsUp className='h-5 mr-1 ml-3' />
-            {result.vote_count}
-          </p>
-        </div>
+          className='sm:rounded-t-lg group-hover:opacity-75'
+        ></Image>
       </Link>
+      <div className='p-2'>
+        <h2 className='text-lg font-bold truncate group-hover:text-amber-600'>
+          {result.title || result.name}
+        </h2>
+        <p className='line-clamp-2 text-md'>{result.overview}</p>
+        <p className='flex items-center'>
+          {result.release_date || result.first_air_date}
+          <FiThumbsUp className={`h-5 ml-3 ${result.vote_count > 2000 ? 'text-amber-500' : ''}`}/>
+            <span className={`${result.vote_count > 2000 ? 'text-amber-500' : ''}`}>{result.vote_count}</span>  
+        </p>
+      </div>
     </div>
-  );
+  )
 }
